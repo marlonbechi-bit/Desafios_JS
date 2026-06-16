a92eaddf16ef10c91a12d368e338e3a5bd182f0f
marlon.bechi-bit <marlon.bechi@gmail.com>
2026-06-15 21:04:43 -0300

    Desafio 02 Concluído

--- Desafio 02/media.js
+++ Desafio 02/media.js
@@ -13,7 +13,7 @@ Média da turma: 10.0
 let notas = [7.5, 8.4, 9.3, 6.2, 5.1];
 let soma = 0;
 
-for (let i = 0; i <= notas.length; i++) {
+for (let i = 0; i < notas.length; i++) {
     soma += notas[i];
 }
 let media = soma / notas.length;
