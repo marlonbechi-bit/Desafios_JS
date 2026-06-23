e3fb9dd9d5505cb5bcdb343f63a5d8d65a418f66
marlon.bechi-bit <marlon.bechi@gmail.com>
2026-06-22 21:46:23 -0300

    Desafio contador concluído

---
+++ Desafio 05/contador.js
@@ -0,0 +1,28 @@
+/*
+DESAFIO 05 - CONTADOR INTELIGENTE
+
+Crie uma função chamada contador() que receba:
+- valor inicial
+- valor final
+
+A função deve exibir todos os números entre os valores informados.
+
+Após concluir, crie uma segunda versão capaz de realizar a contagem regressiva.
+*/
+
+function contador(inicial, final) {
+    console.log("Contador Ascendente");
+for ( let i = inicial; i <= final; i++) {
+    console.log(i);
+}
+}
+contador(0, 10);
+
+function contadorRegressivo(inicial, final) {
+    console.log("Contador Decrescente");
+    for (let i = inicial; i >= final; i--) {
+        console.log(i);
+    }
+}
+
+contadorRegressivo(10, 0);
